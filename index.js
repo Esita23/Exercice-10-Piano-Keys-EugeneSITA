674aa517f45810fb108dea3e0885beef61122d94
Esita23 <135968759+Esita23@users.noreply.github.com>
2023-08-07 08:37:43 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+/*
+NE RIEN MODIFIER ICI
+*/
+/*
+Create projet PianoKeys*/
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log("Application started and Listening on port 3000");
+});
+
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
